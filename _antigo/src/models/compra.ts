5390f71ec9c7f26aa2a37300485fe95e951752d5
LuanSchmidt <luanms10@hotmail.com>
2019-08-28 06:56:25 -0300

    update-compra.ts

--- _antigo/src/models/compra.ts
+++ _antigo/src/models/compra.ts
@@ -1,9 +1,13 @@
-
+import {Cartao} from "src/models/cartao";
+import {Cliente} from "src/models/cliente";
+import {Administrador} from "src/models/administrador";
 
 export class Compra {
     id: number;
     valor_total: number;
     valor_desconto: number;
     registro_ativo: boolean;
-    
+    cliente: Cliente;
+    administrador: Administrador;
+    cartao: Cartao;
 }
