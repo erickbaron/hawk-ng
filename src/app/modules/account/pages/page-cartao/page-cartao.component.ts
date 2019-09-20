c632a3a8923befdd3305132f13efda1d0880d9dc
LuanSchmidt <luanms10@hotmail.com>
2019-09-20 11:14:57 -0300

    desenvolvendo-cartao

--- src/app/modules/account/pages/page-cartao/page-cartao.component.ts
+++ src/app/modules/account/pages/page-cartao/page-cartao.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Cartao } from 'src/models/cartao';
+import { CartaoService } from 'src/services/cartao.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 
 @Component({
@@ -13,6 +15,27 @@ export class PageCartaoComponent implements OnInit{
   cartao: Cartao = new Cartao ();
 
   constructor(
-    private service: 
-  ) { }
+    private service: CartaoService,
+    private route: ActivatedRoute,
+    private router: Router) { }
+
+  ngOnInit() {
+    this.returnUrl = '/compact/account/profile'
+  }
+
+  salvar() {
+    this.service.adicionar(this.cartao).subscribe(x => {
+      this.router.navigateByUrl(this.returnUrl)
+      // sucesso
+      alert("Cartão cadastrado com sucesso.")
+    }, 
+    error => {
+      // erro
+      alert("Não foi possível cadastrar.")
+    })
+  }
+
+    cancelar() {
+      this.router.navigateByUrl(this.returnUrl)
+    }
 }
--- src/services/cartao.service.ts
+++ src/services/cartao.service.ts
@@ -2,5 +2,34 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { Cartao } from 'src/models/cartao';
 
-const 
+const url = environment.url + "/card"
+@Injectable({
+  providedIn: 'root'
+})
+export class CartaoService {
+
+  constructor(private http: HttpClient) { }
+
+  // ajax
+//   obterTodos(): Observable<Cartao[]> {
+//     return this.http.get<Cartao[]>(url + "/obtertodos");
+//   }
+
+//   obterPeloId(id: number): Observable<any> {
+//     return this.http.get<any>(url + "/obterpeloid?id=" + id); 
+//   }
+
+//   alterar(cartao): Observable<Cartao> { 
+//     return this.http.put<Cartao>(url + '/update', cartao);
+//   }
+
+  adicionar(cartao: Cartao): Observable<any> {
+    return this.http.post(url + "/add", cartao);
+  }
+
+//   apagar(id: number): Observable<any> {
+//     return this.http.delete(url + "/delete?id=" + id);
+//   }
+}
