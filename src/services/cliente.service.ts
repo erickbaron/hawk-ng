961b0dd0ee0b3517b45ecb85b0c71758df54f27d
Jonatas Adams da Silva <jonatas.adams@gmail.com>
2019-09-26 07:53:38 -0300

    Revert "Merge branch 'Jonatas' into Igor"
    This reverts commit 2736d853435b2f51ca9fb09523173d077d7a0b2a, reversing
    changes made to 3edb2f60330a61f7a4b403d1d3a3077367f57bbe.

--- src/services/cliente.service.ts
+++ src/services/cliente.service.ts
@@ -4,31 +4,34 @@ import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Cliente } from 'src/models/cliente';
 
-const url = environment.url + "/clientes"
+
+const url = environment.url + '/cliente'
+
 @Injectable({
   providedIn: 'root'
 })
-export class ClienteService {
+export class ProfileService {
 
   constructor(private http: HttpClient) { }
 
   adicionar(cliente: Cliente): Observable<any> {
     return this.http.post(url + "/add", cliente);
+  }
 
+  apagar(id: number): Observable<any> {
+    return this.http.delete(url + "/delete?id=" + id);
   }
 
   alterar(cliente): Observable<Cliente> {
     return this.http.put<Cliente>(url + '/update', cliente);
   }
 
-
   obterTodos(): Observable<Cliente[]> {
     return this.http.get<Cliente[]>(url + "/obtertodos");
   }
 
   obterPeloId(id: number): Observable<any> {
-    return this.http.get<any>(url + "/obterpeloid?id=" + id);
+    return this.http.get<any>(url + "/obterpeloid?id=" + id); 
   }
-
 
 }
