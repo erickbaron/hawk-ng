946a717aabd906d6d2b5a84880b6b877c5a3b7b2
vitor-paulo <vitorpaulo333019@gmail.,com>
2019-09-26 08:24:12 -0300

    Revert "Merge remote-tracking branch 'origin/João' into vitor"
    This reverts commit d6985b16368066535f63943956e18dd53f47d8d3, reversing
    changes made to fe9a298f3f2b1d851b257da40f13948c96254f62.

--- src/app/modules/account/pages/page-profile/page-profile.component.ts
+++ src/app/modules/account/pages/page-profile/page-profile.component.ts
@@ -1,58 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { Cliente } from 'src/models/cliente';
-import { Router, ActivatedRoute } from '@angular/router';
-import { ClienteService } from 'src/services/cliente.service';
-
-
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-page-profile',
     templateUrl: './page-profile.component.html',
     styleUrls: ['./page-profile.component.sass']
 })
-
-
-export class PageProfileComponent implements OnInit {
-   returnUrl: string; 
-   id: number;
-   cliente: Cliente = new Cliente();
-    constructor(
-        private service: ClienteService,
-        private route: ActivatedRoute,
-        private router: Router) { }
-        clientes:   Cliente[] = [];
-
-    ngOnInit() {
-        this.id = parseInt(this.route.snapshot.paramMap.get('id'));
-        this.obterPeloId(this.id);
-        this.returnUrl = "/";
-              }
-            
-    salvar() {
-        this.service.adicionar(this.cliente).subscribe(x => {
-        // sucesso
-        alert("Cadastrou")
-        }, 
-        error => {
-        // erro
-        alert("Não foi possível cadastrar")
-        })
-        }
-    obterPeloId(id) {
-        this.service.obterPeloId(id).subscribe(x => {
-        this.cliente = x;
-        })
-        }
-        
-    alterar(cliente) {
-        this.service.alterar(cliente).subscribe( x => {
-        alert("Registro Alterado com Sucesso")
-        },
-        error => {
-        alert("Não foi possível alterar")
-        })
-       }
-    }  
+export class PageProfileComponent {
+    constructor() { }
 }
-
-
