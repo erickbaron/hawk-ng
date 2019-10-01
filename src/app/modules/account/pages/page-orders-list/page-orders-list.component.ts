31ff3fb48ec6033a06cc2f6adc6e14fb6f5a461f
JoaoVictorDF <joaovdf17@hotmail.com>
2019-10-01 03:52:49 -0300

    commit

--- src/app/modules/account/pages/page-orders-list/page-orders-list.component.ts
+++ src/app/modules/account/pages/page-orders-list/page-orders-list.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Compra } from 'src/models/compra';
 import { CompraService } from 'src/services/compra.service';
-import { Router, ActivatedRoute  } from '@angular/router';
+import { ActivatedRoute, Router} from '@angular/router';
 
 
 @Component({
@@ -10,18 +10,14 @@ import { Router, ActivatedRoute  } from '@angular/router';
     styleUrls: ['./page-orders-list.component.sass']
 })
 export class PageOrdersListComponent {
+    returnUrl: string;
+
     compra: Compra = new Compra();
-    compras: Compra[] = [];
-   
+
     constructor(
         private service: CompraService,
-        private router: Router,
         private route: ActivatedRoute,
-    ) { }
+        private router: Router) { }
 
-    salvar() {
-        this.service.adicionar(this.compra).subscribe(x => {
-            
-        })
-    }
+
 }
